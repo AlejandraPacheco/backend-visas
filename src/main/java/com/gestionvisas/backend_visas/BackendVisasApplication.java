b43c294b18597dcfa9777a9422cf6f80df5bc3dd
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2025-09-20 16:03:36 -0400

    Backend Creation

---
+++ src/main/java/com/gestionvisas/backend_visas/BackendVisasApplication.java
@@ -0,0 +1,13 @@
+package com.gestionvisas.backend_visas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVisasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVisasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionvisas/backend_visas/BackendVisasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionvisas.backend_visas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVisasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
